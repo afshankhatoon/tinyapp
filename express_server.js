761116923eb0066fb084ee9f6e32ad71559c6860
afshankhatoon <afshankhatoon.job@gmail.com>
2020-11-07 21:36:53 +0000

    w03d1 URL shortening-1

--- express_server.js
+++ express_server.js
@@ -8,6 +8,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+    return Math.random().toString(36).substr(2, 6);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -42,6 +49,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let longURL;
   for(let url in urlDatabase){
@@ -50,4 +61,9 @@ app.get("/urls/:shortURL", (req, res) => {
   }
   const templateVars = { shortURL: req.params.shortURL, longURL: longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
